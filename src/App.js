e208cf54a5e997af15a1734f3b429d937d94c097
Paliy Stepan <palstepweb@gmail.com>
2020-05-18 17:50:37 +0300

    add routes and nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Error from "./pages/Error";
+import SingleRoom from "./pages/SingleRoom";
+import {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Resort</h1>
+        <Navbar/>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/" exact component={Rooms} />
+            <Route path="/rooms/:id" exact component={SingleRoom} />
+            <Route  component={Error} />
+        </Switch>
     </div>
   );
 }

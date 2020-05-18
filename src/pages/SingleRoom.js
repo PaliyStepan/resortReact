38a8038558f81da913e3ddc2d98eea73caa399fd
Paliy Stepan <palstepweb@gmail.com>
2020-05-18 12:51:04 +0300

    add main structure
    *images
    *pages folder

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class SingleRoom extends Component{
+   render() {
+       return (
+           <div>
+               <h2>SingleRoom</h2>
+           </div>
+       )
+   }
+}
+
+export default  SingleRoom;

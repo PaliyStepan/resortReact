d00309a5e0d4411c7fa269e9a9273c5570b63560
Paliy Stepan <palstepweb@gmail.com>
2020-05-20 15:12:35 +0300

    refactor roomsContainer

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,17 +1,68 @@
 import React from "react";
 import RoomsList from "./RoomsList";
 import RoomsFilter from "./RoomsFilter";
+import {withRoomConsumer} from "../context/context";
+import Loader from "./Loader";
 
-const RoomsContainer = () => {
-    return(
-        <div className="banner">
-
-        </div>
-
+function RoomsContainer({context}) {
+    const {loading, sortedRooms,rooms } = context;
+    if (loading) {
+        return <Loader/>;
+        }
+    return (
+        <>
+            <RoomsList rooms={rooms} />
+            <RoomsFilter rooms={sortedRooms}/>
+        </>
     )
-};
+}
+
+
+export default withRoomConsumer(RoomsContainer);
 
 
 
 
-export default RoomsContainer;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import RoomsList from "./RoomsList";
+// import RoomsFilter from "./RoomsFilter";
+// import {RoomConsumer} from "../context/context";
+// import Loader from "./Loader";
+//
+// const RoomsContainer = () => {
+//
+//     return(
+//         <RoomConsumer>
+//             {
+//                 (value) =>{
+//                     const {loading, sortedRooms, rooms} = value;
+//
+//                     if (loading) {
+//                         return <Loader/>
+//                     }
+//                     return (
+//                         <div>
+//                             <RoomsList rooms={rooms} />
+//                             <RoomsFilter rooms={sortedRooms}/>
+//                         </div>
+//                     )
+//                 }
+//
+//             }
+//         </RoomConsumer>
+//     )
+// };
+//
+//
+//
+//
+// export default RoomsContainer;

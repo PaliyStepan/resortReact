df7e4b294e4e4b66d7bc9649f11289fc1a30e0c6
Paliy Stepan <palstepweb@gmail.com>
2020-06-02 17:11:33 +0300

    add room type select functional

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,11 +1,40 @@
 import React from "react";
+import {useContext} from "react";
+import {RoomContext} from "../context/context";
+import Title from "./Title";
 
-const RoomsFilter = () => {
+const getUnique = (items,value) =>{
+    return [...new Set(items.map(item=>item[value]))]
+};
+
+
+const RoomsFilter = ({rooms}) => {
+    const context = useContext(RoomContext);
+
+    const {handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets} = context;
+
+    // get uniq types of rooms
+    let types = getUnique(rooms,'type');
+    //ad all
+    types = ['all', ...types];;
+
+    types = types.map((item,index)=>{
+        return <option value={item} key={index}>{item} </option>
+    });
+
+
     return(
-        <div className="banner">
-            <h1>filter</h1>
-        </div>
-
+        <section className="filter-container">
+            <Title title="search rooms"/>
+            <form className="filter-form">
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
+                        {types}
+                    </select>
+                </div>
+            </form>
+        </section>
     )
 };
 
